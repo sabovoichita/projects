e3f7392382033f6458e8306e0d9d0b890dd2dc2b
Voichita Maria <sabo_voichita@yahoo.com>
2024-07-09 14:20:20 +0200

    projects carousel with overlay and colourful dots

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-console.log("welcome!");
+let slideIndex = 1;
 
 function $(selector) {
   return document.querySelector(selector);
@@ -20,8 +20,19 @@ function createHeader() {
 function createContent() {
   console.log("creating content");
   return `
-   <div class="contents"> 
-    </div>
+   
+    <section class="section" id="section--2">
+      <div class="container-portofolio">
+        <h2>Portofolio</h2>
+        <div class="projects">
+          <div class="main__project"></div>
+          <div class="secondary__projects">
+            
+          </div>
+        </div>
+      </div>
+    </section>
+
   `;
 }
 
@@ -30,40 +41,71 @@ function loadProjects() {
   fetch("projects.json").then((r) => {
     r.json().then((projects) => {
       //   console.log("projects %o", projects);
-      createContentJS(projects);
+      createPortofolio(projects);
     });
   });
 }
 
-function createContentJS(projects) {
+function createPortofolio(projects) {
   console.log("in the createContentJS function");
 
   //   console.log(projects);
-  const div = $(".contents");
+  const div = $(".secondary__projects");
   //   console.log("div:", div);
   //   console.log("projects now:", projects);
 
   const text = projects.map((project) => {
     return `
-      <div class="content">
-      <a href="${project.url}"><h2>${project.name}</h2></a>
-      <div class="details">
-        <h3>Technologies used</h3>
-        <ul>
-          <li>${project.tehnologies}</li>
-        </ul>
+    
+  <div class="project--${project.id} mySlides fade" style="background: url(${project.preview})">
+      <div class="overlay_portofolio_projects">
+        <h4 class="overlay_port_title">${project.name}</h4>
+        <div class="description">
+          <h3>Description:</h3>
+          <p>${project.description}</p>
+        </div>
+        <div class="details">
+          <h3>Technologies used: ${project.tehnologies}</h3>
+        </div>
+        <a href="${project.url}">
+          <button class="overlay_port_btn" onclick="${project.url}">
+            View Project
+          </button></a
+        >
       </div>
-      <details>
-        <summary>Preview</summary>
-        <a href="${project.url}"
-          ><img src="${project.preview}" alt="selfie"
-        /></a>
-      </details>
     </div>
+    
       `;
   });
   //   console.log("text", text);
-  div.innerHTML = "<h2>Voichita's Projects</h2>" + text.join("");
+  div.innerHTML =
+    text.join("") +
+    `<a class="prev" onclick="plusSlides(-1)">&#10094;</a>
+    <a class="next" onclick="plusSlides(1)">&#10095;</a>
+    <br />` +
+    `<div class="dots" style="text-align: center">
+        <span class="dot" onclick="currentSlide(1)"></span>
+        <span class="dot" onclick="currentSlide(2)"></span>
+        <span class="dot" onclick="currentSlide(3)"></span>
+        <span class="dot" onclick="currentSlide(4)"></span>
+        <span class="dot" onclick="currentSlide(5)"></span>
+        <span class="dot" onclick="currentSlide(6)"></span>
+      </div>`;
+}
+
+function createSection() {
+  const portofolio = createPortofolio(projects);
+  const text = `
+    <section class="section" id="section--2">
+      <div class="container-portofolio">
+        <h2>Portofolio</h2>
+        <h4>20 projects</h4>
+        <div class="projects">
+          <div class="secondary__projects">${portofolio}</div>
+        </div>
+      </div>
+    </section>`;
+  $("body").innerHTML = text;
 }
 
 function createFooter() {
@@ -94,15 +136,44 @@ function createFooter() {
   `;
 }
 
-function insertContent(projects) {
+function insertContent() {
   console.log("insert content");
   $("body").innerHTML = createHeader() + createContent() + createFooter();
 }
 
+// Next/previous controls
+function plusSlides(n) {
+  showSlides((slideIndex += n));
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides((slideIndex = n));
+}
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+}
 function initEvents() {
   console.log("init Events");
   insertContent();
   loadProjects();
+  showSlides(slideIndex);
 }
 
 initEvents();
